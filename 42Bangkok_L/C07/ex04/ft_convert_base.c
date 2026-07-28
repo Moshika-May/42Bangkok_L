15dd7406b9538d9482d77beee5bfc1bbb68fbf6a
Moshika-May <Color7338@gmail.com>
2026-07-29 00:42:27 +0700

    C07 also I forgor to commit tho

--- 42Bangkok_L/C07/ex04/ft_convert_base.c
+++ 42Bangkok_L/C07/ex04/ft_convert_base.c
@@ -6,13 +6,67 @@
 /*   By: kmahanin <kmahanin@student.42bangkok.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/26 13:26:13 by kmahanin          #+#    #+#             */
-/*   Updated: 2026/07/26 13:33:13 by kmahanin         ###   ########.fr       */
+/*   Updated: 2026/07/28 22:50:53 by kmahanin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 #include <stdlib.h>
 
-char	*ft_convert_base(char *nbr, char *base_form, char *base_to)
+int			base_check_n_len(char *base);
+int			index_base(char c, char *base);
+char		*ft_itoa_base(long long nbr, char *base);
+
+long long	ft_atoi_base(char *str, char *base)
 {
+	unsigned int	i;
+	int				j;
+	long long		k;
+	int				b_len;
+
+	i = 0;
+	j = 1;
+	k = 0;
+	b_len = base_check_n_len(base);
+	if (!b_len)
+		return (0);
+	while (str[i] == ' ' || (str[i] >= '\t' && str[i] <= '\r'))
+		i++;
+	while (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			j = -j;
+		i++;
+	}
+	while (str[i] && index_base(str[i], base) != -1)
+		k = (k * b_len) + index_base(str[i++], base);
+	return (k * j);
 }
+
+char	*ft_convert_base(char *nbr, char *base_from, char *base_to)
+{
+	long long	n;
+
+	if (!nbr || !base_check_n_len(base_from) || !base_check_n_len(base_to))
+		return (NULL);
+	n = ft_atoi_base(nbr, base_from);
+	return (ft_itoa_base(n, base_to));
+}
+/*
+#include <stdio.h>
+
+int	main(int c, char **vec)
+{
+	char	*after;
+
+	after = ft_convert_base(vec[1], vec[2], vec[3]);
+	if (c != 4)
+		return (0);
+	printf("Num B: %s\n", vec[1]);
+	printf("From: %s\n", vec[2]);
+	printf("To: %s\n", vec[3]);
+	printf("Num A: %s\n", after);
+	free(after);
+	return (0);
+}
+*/
--- 42Bangkok_L/C07/ex04/ft_convert_base2.c
+++ 42Bangkok_L/C07/ex04/ft_convert_base2.c
@@ -6,6 +6,95 @@
 /*   By: kmahanin <kmahanin@student.42bangkok.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/26 13:26:19 by kmahanin          #+#    #+#             */
-/*   Updated: 2026/07/26 13:33:21 by kmahanin         ###   ########.fr       */
+/*   Updated: 2026/07/28 22:49:03 by kmahanin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stddef.h>
+#include <stdlib.h>
+
+int	base_check_n_len(char *base)
+{
+	unsigned int	i;
+	unsigned int	j;
+
+	i = 0;
+	if (!base || !base[i] || !base[1])
+		return (i);
+	while (base[i])
+	{
+		if (base[i] == '+' || base[i] == '-' || base[i] == ' '
+			|| (base[i] >= '\t' && base[i] <= '\r'))
+			return (0);
+		j = i + 1;
+		while (base[j])
+		{
+			if (base[i] == base[j])
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	return (i);
+}
+
+int	index_base(char c, char *base)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (base[i])
+	{
+		if (base[i] == c)
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
+static int	get_num_len(long long nbr, int base_len)
+{
+	unsigned int	i;
+
+	i = 0;
+	if (nbr <= 0)
+	{
+		i++;
+		nbr = -nbr;
+	}
+	while (nbr > 0)
+	{
+		nbr /= base_len;
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_itoa_base(long long nbr, char *base)
+{
+	long long		n;
+	unsigned int	b_len;
+	unsigned int	i;
+	char			*arr;
+
+	b_len = base_check_n_len(base);
+	n = nbr;
+	i = get_num_len(n, b_len);
+	arr = (char *)malloc(sizeof(char) * (i + 1));
+	if (!arr)
+		return (NULL);
+	arr[i] = '\0';
+	if (n == 0)
+		arr[0] = base[0];
+	if (n < 0)
+	{
+		arr[0] = '-';
+		n = -n;
+	}
+	while (n > 0)
+	{
+		arr[--i] = base[n % b_len];
+		n /= b_len;
+	}
+	return (arr);
+}
