cee7da07cf6f1f8690bef50cac86962bf9bb998b
Moshika-May <Color7338@gmail.com>
2026-07-29 20:13:30 +0700

    C08

---
+++ 42Bangkok_L/C08/ex00/ft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kmahanin <kmahanin@student.42bangkok.com>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/29 14:41:30 by kmahanin          #+#    #+#             */
+/*   Updated: 2026/07/29 14:43:27 by kmahanin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+# define FT_H
+# include <unistd.h>
+
+void	ft_putchar(char c);
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
+#endif
---
+++ 42Bangkok_L/C08/ex01/ft_boolean.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kmahanin <kmahanin@student.42bangkok.com>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/29 14:44:58 by kmahanin          #+#    #+#             */
+/*   Updated: 2026/07/29 17:10:13 by kmahanin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
+# include <unistd.h>
+
+typedef int	t_bool;
+
+# define TRUE 1
+# define FALSE 0
+
+# define EVEN_MSG "I have an even number of arguments\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
+# define SUCCESS 0
+# define EVEN(nbr) ((nbr) % 2 == 0)
+
+void		ft_putstr(char *str);
+t_bool		ft_is_even(int nbr);
+
+#endif
