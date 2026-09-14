77bfd1620165a551aceee26cbaa69c658097eafa
Moshika-May <Color7338@gmail.com>
2026-09-14 14:20:55 +0700

    it should be fine?

--- 42Bangkok_L/CommonCore_Section/libft/ft_strncmp.c
+++ 42Bangkok_L/CommonCore_Section/libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: kmahanin <kmahanin@student.42bangkok.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/15 17:16:47 by kmahanin          #+#    #+#             */
-/*   Updated: 2026/09/14 13:47:57 by kmahanin         ###   ########.fr       */
+/*   Updated: 2026/09/14 14:19:59 by kmahanin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,15 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	if (n == 0)
-		return (0);
-	while (i < n && s1[i] != '\0' && s1[i] == s2[i])
+	while (i < n && (s1[i] || s2[i]))
+	{
+		if (s1[i] != s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		i++;
-	if (i == n)
-		return (0);
-	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	}
+	return (0);
 }
-
+/*
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -37,3 +37,4 @@ int	main(int ac, char **av)
 	printf("%d\n", ft_strncmp(av[1], av[2], atoi(av[3])));
 	return (0);
 }
+*/
