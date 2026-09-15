bb96182359f5216b5c7d0334d0bc0b1189199393
Moshika-May <Color7338@gmail.com>
2026-09-15 18:20:47 +0700

    wtf is this lnklst

---
+++ 42Bangkok_L/CommonCore_Section/libft/ft_lstnew.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kmahanin <kmahanin@student.42bangkok.com>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/15 15:42:46 by kmahanin          #+#    #+#             */
+/*   Updated: 2026/09/15 18:07:23 by kmahanin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*new;
+
+	new = malloc(sizeof(t_list));
+	if (!new)
+		return (NULL);
+	new->content = content;
+	new->next = NULL;
+	return (new);
+}
---
+++ 42Bangkok_L/CommonCore_Section/libft/libft.h
@@ -0,0 +1,17 @@
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stddef.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}					t_list;
+
+t_list				*ft_lstnew(void *content);
+
+#endif
